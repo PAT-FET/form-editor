f6942af5e5465fd40117b5a410dbd70579972762
Ghu <ghu@glprop.com>
2020-03-25 14:18:06 +0800

    save

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -1,10 +1,6 @@
 import _Vue from 'vue'
 
 declare const _default: {
-  install: (Vue: typeof _Vue, options?: any) => void
-};
+  install: (Vue: typeof _Vue, options?: any) => void;
+}
 export default _default
-
-
-
-
