397f8dfd09cbb0d66bd2536ce530df4a8147660b
Ghu <ghu@glprop.com>
2020-11-12 18:03:41 +0800

    save

--- src/components/fe-designer/display-zone/index.vue
+++ src/components/fe-designer/display-zone/index.vue
@@ -1,7 +1,14 @@
 <template>
 <div :class="[$style.container]">
   <div :class="[$style.content]">
-    <form-instance :def="def" :form-data="formData" design></form-instance>
+    <form-instance :def="def" :form-data="formData" design v-show="!empty"></form-instance>
+    <div v-show="empty">
+      <i :class="[$style.icon]">
+        <svg class="fe-icon" aria-hidden="true">
+          <use :xlink:href="'#iconempty1'"></use>
+        </svg>
+      </i>
+    </div>
   </div>
 </div>
 </template>
@@ -18,6 +25,10 @@ export default class DisplayZone extends Vue {
   @Prop() def!: FormDefinition
 
   formData: any = {}
+
+  get empty () {
+    return this.def?.list?.length < 1
+  }
 }
 </script>
 
@@ -33,5 +44,9 @@ export default class DisplayZone extends Vue {
   border: 1px dashed #999;
   margin: 10px;
   overflow: auto;
+}
+
+.icon {
+  font-size: 96px;
 }
 </style>
